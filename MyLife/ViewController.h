532efa2d1cba1bdd07c131535c8d93b20f7b886e
RyeWhiskeyBourbon <qdfuture0532@vip.qq.com>
2016-03-28 11:07:49 +0800

    Initial Commit

---
+++ MyLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLife
+//
+//  Created by RyeWhiskey on 16/3/28.
+//  Copyright © 2016年 RyeWhiskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyLife/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyLife
+//
+//  Created by RyeWhiskey on 16/3/28.
+//  Copyright © 2016年 RyeWhiskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
